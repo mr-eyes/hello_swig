ae506dbecf7f4b029fba31cb48e85f8e09acd7a9
mabuelanin@gmail.com <mohamed.abuelanin@gmail.com>
2020-07-14 17:46:33 +0200

    edit build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir build && cd build
+cmake .. && make
+cd ..
+
 rm -rf build/temp* build/lib.linux* dist/* __pycache__/ *cxx *pyc interface_wrap.cpp helloSwig.py *so helloSwig.egg-info/ # interface_wrap.cpp #dist/*
 
 $(which python) setup.py bdist_wheel
