c0f3d75b090a31297b5380cce7dae3ddb6e67dd8
mabuelanin@gmail.com <mohamed.abuelanin@gmail.com>
2020-07-14 20:41:44 +0200

    swig

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 echo "Building C++ ..."
 
+PYTHON=/home/travis/miniconda3/envs/test/bin/python
+
 mkdir build && cd build
 cmake .. && make
 cd ..
 
 rm -rf build/temp* build/lib.linux* dist/* __pycache__/ *cxx *pyc interface_wrap.cpp helloSwig.py *so helloSwig.egg-info/ # interface_wrap.cpp #dist/*
 
-$(which python) setup.py bdist_wheel
+$PYTHON setup.py bdist_wheel
 
 cd dist/
 
-$(which python) -m pip uninstall helloSwig -y
+$PYTHON -m pip uninstall helloSwig -y
 
-$(which python) -m pip install helloSwig*cp*.whl
+$PYTHON -m pip install helloSwig*cp*.whl
 
 cd ..
 
